d5c92a0428c91f3ec9468bfd987993638e551b82
Mike Gehard <mike.gehard@gmail.com>
2014-03-10 10:13:38 -0600

    Refactor to make more clear

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -1,48 +1,24 @@
-input = gets.chomp.to_i
-
-roman_numerals = {
-    1 => "I",
-    5 => "V",
+ROMAN_NUMERALS = {
+    10 => "X",
     9 => "IX",
-    10 => "X"
+    5 => "V",
+    1 => "I",
 }
+
+input = gets.chomp.to_i
 
 output = ""
 
 while (input > 0)
-  # do something here
-  number_of_tens = input / 10
+  ROMAN_NUMERALS.each do |key, value|
+    number_of_tens = input / key
 
-  number_of_tens.times do
-    output << roman_numerals[10]
+    number_of_tens.times do
+      output << value
+    end
+
+    input -= number_of_tens * key
   end
-
-  input -= number_of_tens * 10
-
-  number_of_nines = input / 9
-
-  number_of_nines.times do
-    output << roman_numerals[9]
-  end
-
-  input -= number_of_nines * 9
-
-  number_of_fives = input / 5
-
-  number_of_fives.times do
-    output << roman_numerals[5]
-  end
-
-  input -= number_of_fives * 5
-
-  number_of_ones = input / 1
-
-  number_of_ones.times do
-    output << roman_numerals[1]
-  end
-
-  input -= number_of_ones * 1
 end
-
 
 puts output
