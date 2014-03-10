97f03fdfbee9fe42df893714d41f98f8b83fd82c
Mike Gehard <mike.gehard@gmail.com>
2014-03-10 10:04:55 -0600

    Encode 9 and 39 into roman numerals

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -1,1 +1,48 @@
-puts "V"
+input = gets.chomp.to_i
+
+roman_numerals = {
+    1 => "I",
+    5 => "V",
+    9 => "IX",
+    10 => "X"
+}
+
+output = ""
+
+while (input > 0)
+  # do something here
+  number_of_tens = input / 10
+
+  number_of_tens.times do
+    output << roman_numerals[10]
+  end
+
+  input -= number_of_tens * 10
+
+  number_of_nines = input / 9
+
+  number_of_nines.times do
+    output << roman_numerals[9]
+  end
+
+  input -= number_of_nines * 9
+
+  number_of_fives = input / 5
+
+  number_of_fives.times do
+    output << roman_numerals[5]
+  end
+
+  input -= number_of_fives * 5
+
+  number_of_ones = input / 1
+
+  number_of_ones.times do
+    output << roman_numerals[1]
+  end
+
+  input -= number_of_ones * 1
+end
+
+
+puts output
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -10,7 +10,6 @@ describe "RomanNumerals" do
   end
 
   it "handles the numbers that are just below the next boundary" do
-    pending
     run_script("roman_numerals.rb")
     type("9")
 
@@ -19,7 +18,6 @@ describe "RomanNumerals" do
   end
 
   it "converts a double digit number to traditional roman numeral" do
-    pending
     run_script("roman_numerals.rb")
     type("39")
 
